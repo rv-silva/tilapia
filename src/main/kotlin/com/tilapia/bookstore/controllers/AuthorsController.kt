facf4a65f219d3be50c115747c3b999766baef78
rv-silva <robertovieirah@hotmail.com>
2024-09-12 15:36:40 +0200

    feat: mockk & tests

--- src/main/kotlin/com/tilapia/bookstore/controllers/AuthorsController.kt
+++ src/main/kotlin/com/tilapia/bookstore/controllers/AuthorsController.kt
@@ -1,9 +1,13 @@
 package com.tilapia.bookstore.controllers
 
 import com.tilapia.bookstore.domain.dto.AuthorDto
+import com.tilapia.bookstore.domain.entities.AuthorEntity
 import com.tilapia.bookstore.services.AuthorService
 import com.tilapia.bookstore.toAuthorDto
 import com.tilapia.bookstore.toAuthorEntity
+import org.springframework.http.HttpStatus
+import org.springframework.http.HttpStatusCode
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
@@ -12,10 +16,11 @@ import org.springframework.web.bind.annotation.RestController
 class AuthorsController(private val authorService: AuthorService) {
 
     @PostMapping(path = ["/v1/authors"])
-    fun createAuthor(@RequestBody authorDto: AuthorDto): AuthorDto {
-        return authorService.save(
+    fun createAuthor(@RequestBody authorDto: AuthorDto): ResponseEntity<AuthorDto> {
+        val createdAuthor = authorService.save(
             authorDto.toAuthorEntity()
         ).toAuthorDto()
+        return ResponseEntity(createdAuthor, HttpStatus.CREATED)
     }
 
 }
---
+++ src/test/kotlin/com/tilapia/bookstore/TestDataUtil.kt
@@ -0,0 +1,20 @@
+package com.tilapia.bookstore
+
+import com.tilapia.bookstore.domain.dto.AuthorDto
+import com.tilapia.bookstore.domain.entities.AuthorEntity
+
+fun testAuthorDtoA(id: Long? = null) = AuthorDto(
+    id = id,
+    name = "John Doe",
+    age = 30,
+    description = "Some description",
+    image = "author-image.jpeg",
+)
+
+fun testAuthorEntityA(id: Long? = null) = AuthorEntity(
+    id = id,
+    name = "John Doe",
+    age = 30,
+    description = "Some description",
+    image = "author-image.jpeg",
+)

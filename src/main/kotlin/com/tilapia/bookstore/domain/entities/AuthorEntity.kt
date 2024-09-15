ca8a3daac64151f2a979e4d2985e72c304894250
rv-silva <robertovieirah@hotmail.com>
2024-09-15 17:11:35 +0200

    feat: query books by author - test

--- src/main/kotlin/com/tilapia/bookstore/domain/entities/AuthorEntity.kt
+++ src/main/kotlin/com/tilapia/bookstore/domain/entities/AuthorEntity.kt
@@ -26,4 +26,30 @@ data class AuthorEntity(
 
     @OneToMany(mappedBy = "authorEntity", cascade = [CascadeType.REMOVE])
     val bookEntities: List<BookEntity> = emptyList(),
-)
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as AuthorEntity
+
+        if (id != other.id) return false
+        if (name != other.name) return false
+        if (age != other.age) return false
+        if (description != other.description) return false
+        if (image != other.image) return false
+//        if (bookEntities != other.bookEntities) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = id?.hashCode() ?: 0
+        result = 31 * result + name.hashCode()
+        result = 31 * result + age
+        result = 31 * result + description.hashCode()
+        result = 31 * result + image.hashCode()
+        result = 31 * result + bookEntities.hashCode()
+        return result
+    }
+}
--- src/test/kotlin/com/tilapia/bookstore/services/impl/BookServiceImplTest.kt
+++ src/test/kotlin/com/tilapia/bookstore/services/impl/BookServiceImplTest.kt
@@ -82,4 +82,29 @@ class BookServiceImplTest @Autowired constructor(
         assertThat(result[0]).isEqualTo(savedBook)
     }
 
+    @Test
+    fun `test that list returns no books when the author ID does not match`() {
+        val savedAuthor = authorRepository.save(testAuthorEntityA())
+        assertThat(savedAuthor).isNotNull()
+
+        val savedBook = bookRepository.save(testBookEntityA(BOOK_A_ISBN, savedAuthor))
+        assertThat(savedBook).isNotNull()
+
+        val result = underTest.list(authorId = savedAuthor.id!! + 1)
+        assertThat(result).hasSize(0)
+    }
+
+    @Test
+    fun `test that list returns books when the author ID does match`() {
+        val savedAuthor = authorRepository.save(testAuthorEntityA())
+        assertThat(savedAuthor).isNotNull()
+
+        val savedBook = bookRepository.save(testBookEntityA(BOOK_A_ISBN, savedAuthor))
+        assertThat(savedBook).isNotNull()
+
+        val result = underTest.list(authorId = savedAuthor.id)
+        assertThat(result).hasSize(1)
+        assertThat(result[0]).isEqualTo(savedBook)
+    }
+
 }
